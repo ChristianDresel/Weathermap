bf2daa0ef3d38057a7a288949d7cd49e8ab6f62c
Christian Dresel <fff@chrisi01.de>
2018-01-20 01:02:37 +0100

    Add Aquarius

--- weathermap/start.sh
+++ weathermap/start.sh
@@ -7,3 +7,4 @@ php ./weathermap --config fff-gw-cd1.conf --output fff-gw-cd1.png --htmloutput f
 php ./weathermap --config nue2.conf --output nue2.png --htmloutput nue2.html
 php ./weathermap --config euserv.conf --output euserv.png --htmloutput euserv.html
 php ./weathermap --config fff-gw-reddog1.conf --output fff-gw-reddog1.png --htmloutput fff-gw-reddog1.html
+php ./weathermap --config fff-gw-aquarius.conf --output fff-gw-aquarius.png --htmloutput fff-gw-aquarius.html
