c51bd0b526a7cb86cf17f63ff458beff8364fc10
Christian Dresel <fff@chrisi01.de>
2018-03-05 09:56:42 +0100

    Neue Aufteilung
    Signed-off-by: Christian Dresel <fff@chrisi01.de>

--- weathermap/start.sh
+++ weathermap/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /var/www/html/rf/weathermap
-php ./weathermap --config l3.conf --output mymap.png --htmloutput l3.html
-php ./weathermap --config l3.conf --output mymap.png --htmloutput index.html
-php ./weathermap --config l3.conf --output l3.png
+php ./weathermap --config l3new.conf --output mymap.png --htmloutput l3.html
+php ./weathermap --config l3new.conf --output mymap.png --htmloutput index.html
+php ./weathermap --config l3new.conf --output l3.png
 php ./weathermap --config fff-gw-cd1.conf --output fff-gw-cd1.png --htmloutput fff-gw-cd1.html
 php ./weathermap --config nue2.conf --output nue2.png --htmloutput nue2.html
 php ./weathermap --config euserv.conf --output euserv.png --htmloutput euserv.html
